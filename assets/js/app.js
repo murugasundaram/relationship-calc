6911997835aa97878d710dfaf216ea01f02bbdf3
Muruga <murugasundaram@expsoltechs.com>
2021-10-18 13:43:40 +0530

    Added a validation and result display done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,68 @@
 let app = new Vue({
     el: '#appRoot',
     data: {
-        firstName : "bas",
-        secondName : "vino",
-        result: ""
+        firstName : "",
+        secondName : "",
+        result: {
+            name: "",
+            icon: ""
+        },
+        error : {
+            firstName : false,
+            secondName : false
+        }
     },
     methods: {
         resetData(){
             this.firstName = ""
             this.secondName = ""
+            this.error.firstName = false
+            this.error.secondName = false
+            this.result.name = ""
+            this.result.icon = ""
         },
         playTheGame() {
-            if(this.firstName == "" || this.secondName == "") {
-                alert('Please provide the names')
+        
+            if(this.firstName == "")
+                this.error.firstName = true
+            else
+                this.error.firstName = false
+
+            if(this.secondName == "")
+                this.error.secondName = true
+            else
+                this.error.secondName = false
+
+            if(this.firstName == "" || this.secondName == "") 
                 return false
-            }
 
             let reLength = this.findDiff(this.firstName, this.secondName)
             let theLetter = this.findFlames(reLength) 
             let flamesObj = {
-                'f' : 'Friend',
-                'l' : 'Love',
-                'a' : 'Affection',
-                'm' : 'Marriage',
-                'e' : 'Enemy',
-                's' : 'Sister',
+                'f' : {
+                    name : 'Friend',
+                    icon : 'far fa-laugh fa-3x text-primary'
+                },
+                'l' : {
+                    name : 'Love',
+                    icon : 'far fa-grin-hearts fa-3x text-success'
+                },
+                'a' : {
+                    name : 'Affection',
+                    icon : 'far fa-kiss fa-3x text-success'
+                },
+                'm' : {
+                    name : 'Marriage',
+                    icon : 'far fa-kiss-wink-heart fa-3x text-success'
+                },
+                'e' : {
+                    name : 'Enemy',
+                    icon : 'far fa-tired fa-3x text-danger'
+                },
+                's' : {
+                    name : 'Sister',
+                    icon : 'far fa-flushed fa-3x text-warning'
+                },
             }
 
             this.result = flamesObj[theLetter]
